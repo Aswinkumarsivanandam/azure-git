9fe122e3cdaf4d3485894c86b055698fa2e20859
aswin <aswins@excelenciaconsulting.com>
2021-08-13 13:04:20 -0500

    test1

---
+++ TestApp/TestApp.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestApp.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
